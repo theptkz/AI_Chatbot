89c1fc4002dfa96fd49e09658808d343c2cd261e
Nguyen Phu Thien <thieng4010@gmail.com>
2021-08-04 14:42:34 +0700

    Add user login authentication + nginx + Dockerize the project

--- frontend/src/Urls.js
+++ frontend/src/Urls.js
@@ -1,7 +1,30 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import Home from "./components/Home";
+import PasswordUpdate from "./components/PasswordUpdate";
+
+// A wrapper for <Route> that redirects to the login screen if you're not yet authenticated.
+function PrivateRoute({ isAuthenticated, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login/",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 function Urls(props) {
   return (
@@ -12,6 +35,16 @@ function Urls(props) {
             {" "}
             <Login {...props} />
           </Route>
+          <PrivateRoute exact path='/' isAuthenticated={props.isAuthenticated}>
+            <Home {...props} />
+          </PrivateRoute>
+          <PrivateRoute
+            exact
+            path='/update_password/'
+            isAuthenticated={props.isAuthenticated}
+          >
+            <PasswordUpdate {...props} />
+          </PrivateRoute>
         </Switch>
       </BrowserRouter>
     </div>
